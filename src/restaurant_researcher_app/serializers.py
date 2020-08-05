215e3112e9b47577b440e5f1e3d28423cad177f6
kathryn0908 <kathrynlael@gmail.com>
2020-08-05 10:36:15 -0600

    update RestaurantSerializer model

--- src/restaurant_researcher_app/serializers.py
+++ src/restaurant_researcher_app/serializers.py
@@ -5,6 +5,7 @@ from django.contrib.auth.hashers import make_password
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Restaurant
+        
         fields = ('id','thumb', 'name', 'cuisines', 'timings', 'url', 'address', 'phone_number', 'has_online_delivery', 'is_delivering_now', 'average_cost_for_two', 'highlights', 'favorites', 'reviews', 'images')
         depth = 3
 
